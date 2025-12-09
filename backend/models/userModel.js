4375686a60092ff6ffeccbae0d20265ea0af77d4
Exiled <joeyknowleschamplain2@gmail.com>
2025-12-09 18:30:46 -0500

    Potential fix for error "Next is not a function"

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -38,12 +38,13 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 // Encrypt password using bcrypt
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
+  if (!this.isModified('password') || !this.password) {
+    return next(); // make sure to return!
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
